6f0845cd6a035666231b062144ed9b532101edda
Famil <frostgamingrespawn@gmail.com>
2020-09-19 02:05:50 +0200

    functionality added to buttons

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = ["Soft": 3, "Medium": 420, "Hard": 720];
+    
+    var secondsRemaining = 60;
+    var timer = Timer();
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate();
+        
+        let hardness = sender.currentTitle!;
+        
+        secondsRemaining = eggTimes[hardness]!;
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true);
+        
+    }
+    
+    @objc func updateTimer() {
+        if(secondsRemaining > 0){
+            print("\(secondsRemaining) seconds.");
+            secondsRemaining-=1;
+        }else {
+            timer.invalidate();
+            label.text = "Done";
+        }
+    }
+    
+    
 
 }
